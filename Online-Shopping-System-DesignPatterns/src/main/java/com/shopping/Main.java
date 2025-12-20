7d16d7c0db401ef90f912d8c52efa098bdb06232
Salma Ahmed <153859562+SalmaAhmed162@users.noreply.github.com>
2025-12-20 13:59:07 +0200

    FinalMain

--- Online-Shopping-System-DesignPatterns/src/main/java/com/shopping/Main.java
+++ Online-Shopping-System-DesignPatterns/src/main/java/com/shopping/Main.java
@@ -8,8 +8,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Set default theme (Light). You can switch later from the UI.
-            ThemeManager.getInstance().setDark(false, new JFrame()); // initialize LAF
+            // Set default theme (Light)
+            ThemeManager.getInstance().setDark(false, new JFrame()); 
             new LoginFrame().setVisible(true);
         });
     }
